ff6bbf516e7b7afcfc328fdcba5baa31266e60a8
godkvng <stryder7x@protonmail.com>
2023-07-20 14:28:48 +0100

    Added feature to add new items for for sale

--- src/base/item/urls.py
+++ src/base/item/urls.py
@@ -4,5 +4,6 @@ from . import views
 # app_name = 'item'
 
 urlpatterns = [
+	path('new/', views.new_item, name='new-item'),
 	path('<str:pk>/', views.detail, name='detail'),
 ]
--- src/base/item/views.py
+++ src/base/item/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Item
+from .forms import NewItemForm
 
 
 def detail(request, pk):
@@ -10,3 +13,22 @@ def detail(request, pk):
 		'related_items': related_items,
 	}
 	return render(request, 'item/detail.html', context)
+
+
+@login_required
+def new_item(request):
+	if request.method == 'POST':
+		form = NewItemForm(request.POST, request.FILES)
+		if form.is_valid():
+			item = form.save(commit=False)
+			item.created_by = request.user
+			item.save()
+
+			return redirect('detail', pk=item.id)
+	else:
+		form = NewItemForm()
+	context = {
+		'form': form,
+		'title': 'New Item',
+	}
+	return render(request, 'item/new_item.html', context)
